a9a571e4bff6c1e866bd9e204af3943337dd3c8f
Palak Singhania <palak.singhania_cs.ccv19@gla.ac.in>
2022-07-04 23:56:09 +0530

    started two new questions

---
+++ src/datastructures/level2/AProblemOnSticks.java
@@ -0,0 +1,16 @@
+/*
+ * Author Name: Palak Singhania
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 04-07-2022
+ */
+
+package datastructures.level2;
+
+import java.util.Scanner;
+
+public class AProblemOnSticks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
