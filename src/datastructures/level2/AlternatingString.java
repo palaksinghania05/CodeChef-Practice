46db0da831d0355968972048f907bd6cfed0e2e2
Palak Singhania <palak.singhania_cs.ccv19@gla.ac.in>
2022-07-07 23:48:13 +0530

    started AlternatingString.java

---
+++ src/datastructures/level2/AlternatingString.java
@@ -0,0 +1,16 @@
+/*
+ * Author Name: Palak Singhania
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 07-07-2022
+ */
+
+package datastructures.level2;
+
+import java.util.Scanner;
+
+public class AlternatingString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
