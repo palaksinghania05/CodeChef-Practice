dbf7347dc7723507cd6b9af3ccd6430d74eadaf9
Palak Singhania <palak.singhania_cs.ccv19@gla.ac.in>
2022-07-08 23:44:04 +0530

    started two questions

---
+++ src/datastructures/level2/MakeAllEqualUsingPairs.java
@@ -0,0 +1,13 @@
+/*
+ * Author Name: Palak Singhania
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 27-06-2022
+ */
+
+package datastructures.level2;
+
+public class MakeAllEqualUsingPairs {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/datastructures/level2/ZeroOnesEqualOneZeroes.java
@@ -0,0 +1,13 @@
+/*
+ * Author Name: Palak Singhania
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 20-06-2022
+ */
+
+package datastructures.level2;
+
+public class ZeroOnesEqualOneZeroes {
+    public static void main(String[] args) {
+
+    }
+}
