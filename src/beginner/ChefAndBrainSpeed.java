f58a22fd1d1d28bf11d479ab07d0c60b4ed52ef1
Palak Singhania <palak.singhania_cs.ccv19@gla.ac.in>
2022-07-14 23:56:45 +0530

    solved one beginner question

---
+++ src/beginner/ChefAndBrainSpeed.java
@@ -0,0 +1,21 @@
+/*
+ * Author Name: Palak Singhania
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 14-07-2022
+ */
+
+package beginner;
+
+import java.util.Scanner;
+
+public class ChefAndBrainSpeed {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+        if(Y <= X)
+            System.out.println("NO");
+        else
+            System.out.println("YES");
+    }
+}
