e8f8749724e84b97135f82f75449bb99afeeab3d
Palak Singhania <palak.singhania_cs.ccv19@gla.ac.in>
2022-07-30 23:56:56 +0530

    solved one beginner question

---
+++ src/beginner/EmailReminders.java
@@ -0,0 +1,18 @@
+/*
+ * Author Name: Palak Singhania
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 30-07-2022
+ */
+
+package beginner;
+
+import java.util.Scanner;
+
+public class EmailReminders {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int U = sc.nextInt();
+        System.out.println(N-U);
+    }
+}
