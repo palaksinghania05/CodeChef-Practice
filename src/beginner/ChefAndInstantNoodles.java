41a2629dc13b5de8fc89ca13d7c1d935bbc10b49
palak singhania <palak.singhania_cs.ccv19@gla.ac.in>
2022-08-23 23:48:44 +0530

    solved two beginner question

---
+++ src/beginner/ChefAndInstantNoodles.java
@@ -0,0 +1,18 @@
+/*
+ * Author Name: Palak Singhania
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 23-08-2022
+ */
+
+package beginner;
+
+import java.util.Scanner;
+
+public class ChefAndInstantNoodles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+        System.out.println(X*Y);
+    }
+}
