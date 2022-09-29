ee1bed22f6791c4c719d5267bda2baacf7790932
palak singhania <palak.singhania_cs.ccv19@gla.ac.in>
2022-09-29 22:42:27 +0530

    solved two beginner question

---
+++ src/beginner/MarioAndTransformation.java
@@ -0,0 +1,28 @@
+/*
+ * Author Name: Palak Singhania
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 29-09-2022
+ */
+
+package beginner;
+
+import java.util.Scanner;
+
+public class MarioAndTransformation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T>0){
+            int X = sc.nextInt();
+            if(X%3==0){
+                System.out.println("normal");
+            }
+            else if(X%3==1){
+                System.out.println("huge");
+            }
+            else
+                System.out.println("small");
+            T--;
+        }
+    }
+}
