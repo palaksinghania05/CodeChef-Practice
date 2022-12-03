6f7acb4eb740cdfdde90f804e9a12e499a18abed
palak singhania <palak.singhania_cs.ccv19@gla.ac.in>
2022-12-03 22:17:10 +0530

    added two question

---
+++ src/beginner/ChefOnVacation.java
@@ -0,0 +1,26 @@
+/*
+ * Author Name: Palak Singhania
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 01-12-2022
+ */
+
+package beginner;
+
+import java.util.Scanner;
+
+public class ChefOnVacation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+            int Z = sc.nextInt();
+            if (X + Y <= Z)
+                System.out.println("Yes");
+            else
+                System.out.println("No");
+            T--;
+        }
+    }
+}
