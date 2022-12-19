8267305928621f4d139a0b31df0ef7187fa7cd30
palak singhania <palak.singhania_cs.ccv19@gla.ac.in>
2022-12-19 20:31:39 +0530

    modified one question

--- src/beginner/AkashAndMissingClass.java
+++ src/beginner/AkashAndMissingClass.java
@@ -6,8 +6,22 @@
 
 package beginner;
 
+import java.util.Scanner;
+
 public class AkashAndMissingClass {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            int result = N / 7;
+            int remainder = N % 7;
+            if (remainder == 6) {
+                System.out.println(result + 1);
+            } else {
+                System.out.println(result);
+            }
+            T--;
+        }
     }
 }
