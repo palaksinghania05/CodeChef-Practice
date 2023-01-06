6e78d30f42d577a6b3a6a6bdfe9f5af3b1e5fb09
palak singhania <palak.singhania_cs.ccv19@gla.ac.in>
2023-01-06 23:56:39 +0530

    added one question

---
+++ src/beginner/VisheshAndHisPopcornCombo.java
@@ -0,0 +1,13 @@
+/*
+ * Author Name: Palak Singhania
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 06-01-2023
+ */
+
+package beginner;
+
+public class VisheshAndHisPopcornCombo {
+    public static void main(String[] args) {
+
+    }
+}
