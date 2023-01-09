891a09ca74ea284cdeefc2827b2bfefe7c1fe30e
palak singhania <palak.singhania_cs.ccv19@gla.ac.in>
2023-01-09 23:58:22 +0530

    added one question

---
+++ src/beginner/KeplersLaw.java
@@ -0,0 +1,31 @@
+/*
+ * Author Name: Palak Singhania
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 07-01-2023
+ */
+
+package beginner;
+
+import java.util.Scanner;
+
+public class KeplersLaw {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int T1 = sc.nextInt();
+            int T2 = sc.nextInt();
+            int R1 = sc.nextInt();
+            int R2 = sc.nextInt();
+
+            double k1 = (1.0 * T1 * T1) / (R1 * R1 * R1);
+            double k2 = (1.0 * T2 * T2) / (R2 * R2 * R2);
+            if (k2 == k1)
+                System.out.println("Yes");
+            else
+                System.out.println("No");
+            T--;
+        }
+    }
+
+}
