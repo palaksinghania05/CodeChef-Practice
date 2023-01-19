d84ebb4e84179f25a8008d695cc4046398a617ab
palak singhania <palak.singhania_cs.ccv19@gla.ac.in>
2023-01-19 23:47:44 +0530

    modified one question and added one question

--- src/beginner1/PackagingCupcakes.java
+++ src/beginner1/PackagingCupcakes.java
@@ -1,23 +1,20 @@
 /*
  * Author Name: Palak Singhania
  * IDE: IntelliJ IDEA Ultimate Edition
- * Date: 06-01-2023
+ * Date: 19-01-2023
  */
 
-package beginner;
+package beginner1;
 
 import java.util.Scanner;
 
-public class VisheshAndHisPopcornCombo {
+public class PackagingCupcakes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         while (T > 0) {
-            int result = 0;
-            for (int i = 0; i < 3; i++) {
-                result = Math.max(result, sc.nextInt() + sc.nextInt());
-            }
-            System.out.println(result);
+            int N = sc.nextInt();
+            System.out.println((N / 2) + 1);
             T--;
         }
     }
