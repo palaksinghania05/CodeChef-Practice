3de54f693624ff2318b0b18c19d91b0915a7d270
palak singhania <palak.singhania_cs.ccv19@gla.ac.in>
2023-02-02 23:57:12 +0530

    added one question

---
+++ src/beginner1/ChangeIt.java
@@ -0,0 +1,13 @@
+/*
+ * Author Name: Palak Singhania
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 02-02-2023
+ */
+
+package beginner1;
+
+public class ChangeIt {
+    public static void main(String[] args) {
+
+    }
+}
