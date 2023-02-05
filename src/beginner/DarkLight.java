c99f8f868e5074860b9b427449492fd0bc0d913b
Palak Singhania <singhaniapalak2002@gmail.com>
2023-02-05 17:16:41 +0530

    added two questions

--- src/beginner/DarkLight.java
+++ src/beginner/DarkLight.java
@@ -6,5 +6,29 @@
 
 package beginner;
 
+import java.util.Scanner;
+
 public class DarkLight {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+            if (K == 0) {
+                if (N % 4 == 0) {
+                    System.out.println("Off");
+                } else {
+                    System.out.println("On");
+                }
+            } else {
+                if (N % 4 == 0) {
+                    System.out.println("On");
+                } else {
+                    System.out.println("Ambiguous");
+                }
+            }
+            T--;
+        }
+    }
 }
