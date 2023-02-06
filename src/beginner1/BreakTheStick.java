e3037847a3d8b691b48e81c9a866ba917a82188f
Palak Singhania <singhaniapalak2002@gmail.com>
2023-02-06 21:04:06 +0530

    modified one question

--- src/beginner1/BreakTheStick.java
+++ src/beginner1/BreakTheStick.java
@@ -31,21 +31,21 @@ public class BreakTheStick {
 
 /*
 The first thing that we have to note is that we initially can break the stick in whatever length we wish.
-Since X < N, we can directly break the stick to the target length, i.e to X. From there on, we can make multiple observations:
-1. If the inital length (N) of the stick was even and the target length (X) is also even, we are left with
+Since X < N, we can directly break the stick to the target length, i.e. to X. From there on, we can make multiple observations:
+1. If the initial length (N) of the stick was even and the target length (X) is also even, we are left with
 two sticks of even length, namely X and N-X. Since the two sticks have the same parity, we can stop breaking
 the sticks further down.
 
-2. If the inital length (N) of the stick was even and the target length (X) is odd, we are left with two
+2. If the initial length (N) of the stick was even and the target length (X) is odd, we are left with two
  sticks of odd length, namely X and N-X. Since the two sticks have the same parity, we can stop breaking
  the sticks further down.
 
-3. If the inital length (N) of the stick was odd and the target length (X) is also odd, we are left with
+3. If the initial length (N) of the stick was odd and the target length (X) is also odd, we are left with
 one stick of odd length (X) and one stick of even length (X-N). Since X-N is even, this means that we can
 keep breaking X-N in two (this is equivalent with dividing by two) until we get all sticks of length 1, thus
 achieving the same parity (= odd) for every stick.
 
-4. If the inital length (N) of the stick was odd and the target length (X) is even, we are left with one
+4. If the initial length (N) of the stick was odd and the target length (X) is even, we are left with one
 stick of even length (X) and one stick of odd length (X-N). Since X-N cannot be further broken down in a way
 that every stick will have an even parity (same parity as X), we conclude that Chef cannot obtain a stick of
 length X given the constraint of parity.
