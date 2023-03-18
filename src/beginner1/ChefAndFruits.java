3207fa219ef051f3b104f559d624275d1ecfbcfe
Palak Singhania <singhaniapalak2002@gmail.com>
2023-03-18 21:24:32 +0530

    modified two question

--- src/beginner1/ChefAndFruits.java
+++ src/beginner1/ChefAndFruits.java
@@ -1,4 +1,21 @@
 package beginner1;
 
+import java.util.Scanner;
+
 public class ChefAndFruits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            int K = sc.nextInt();
+            int difference = Math.abs(N - M);
+            if (difference <= K)
+                System.out.println(0);
+            else
+                System.out.println(difference - K);
+            T--;
+        }
+    }
 }
--- src/beginner1/MathisonAndPangrams.java
+++ src/beginner1/MathisonAndPangrams.java
@@ -1,4 +1,25 @@
 package beginner1;
 
+import java.util.Scanner;
+
 public class MathisonAndPangrams {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int[] prices = new int[26];
+            for (int i = 0; i < 26; i++) {
+                prices[i] = sc.nextInt();
+            }
+            String alphabets = "abcdefghijklmnopqrstuvwxyz";
+            String inputText = sc.next();
+            int result = 0;
+            for (int i = 0; i < 26; i++) {
+                if (!inputText.contains(String.valueOf(alphabets.charAt(i))))
+                    result += prices[i];
+            }
+            System.out.println(result);
+            T--;
+        }
+    }
 }
