eff526a01a871d7673363a60c2b0ce61834f4401
Palak Singhania <singhaniapalak2002@gmail.com>
2023-03-30 23:53:24 +0530

    modified one question

--- src/beginner1/TwoDishes.java
+++ src/beginner1/TwoDishes.java
@@ -1,4 +1,33 @@
 package beginner1;
 
+import java.util.Scanner;
+
 public class TwoDishes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            int C = sc.nextInt();
+            int dishes = 0;
+            while (B > 0) {
+                if (A > 0) {
+                    dishes++;
+                    A--;
+                } else if (C > 0) {
+                    dishes++;
+                    C--;
+                }
+                B--;
+            }
+            if (dishes >= N) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+            T--;
+        }
+    }
 }
