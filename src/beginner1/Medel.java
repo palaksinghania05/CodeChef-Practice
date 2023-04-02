b079a92d7290d2e5f8a0d3960ac34f8b2eb90e82
Palak Singhania <singhaniapalak2002@gmail.com>
2023-04-02 23:57:55 +0530

    modified two question

--- src/beginner1/Medel.java
+++ src/beginner1/Medel.java
@@ -1,4 +1,29 @@
 package beginner1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Medel {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            int[] arr = new int[N];
+            ArrayList<Integer> a = new ArrayList<>();
+            for (int i = 0; i < N; i++) {
+                arr[i] = sc.nextInt();
+                a.add(arr[i]);
+            }
+            Arrays.sort(arr);
+            int max = a.indexOf(arr[0]);
+            int min = a.indexOf(arr[N - 1]);
+            if (max < min)
+                System.out.println(arr[0] + " " + arr[N - 1]);
+            else
+                System.out.println(arr[N - 1] + " " + arr[0]);
+            T--;
+        }
+    }
 }
--- src/beginner1/MultipleChoiceExam.java
+++ src/beginner1/MultipleChoiceExam.java
@@ -1,4 +1,32 @@
 package beginner1;
 
+import java.util.Scanner;
+
 public class MultipleChoiceExam {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            String S = sc.next();
+            String U = sc.next();
+            int count = 0;
+            for (int j = 0; j < N; j++) {
+                if (S.charAt(j) != U.charAt(j) && j < N - 1 && U.charAt(j) != 'N') {
+                    j++;
+                } else if (U.charAt(j) == 'N') {
+                    continue;
+                } else if (S.charAt(j) == U.charAt(j)) {
+                    count++;
+                }
+                if (S.charAt(j) != U.charAt(j) && j == N - 1) {
+                    break;
+                } else if (U.charAt(j) == 'N' && j == N - 1) {
+                    break;
+                }
+            }
+            System.out.println(count);
+            T--;
+        }
+    }
 }
