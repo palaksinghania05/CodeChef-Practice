8b1372c7ae95b1cc074cfc144ab1385761652fa1
Palak Singhania <singhaniapalak2002@gmail.com>
2023-04-19 00:06:27 +0530

    added one question

---
+++ src/beginner1/ANDPlusOR.java
@@ -0,0 +1,15 @@
+package beginner1;
+
+import java.util.Scanner;
+
+public class ANDPlusOR {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            long N = sc.nextLong();
+            System.out.println(0 + " " + N);
+            T--;
+        }
+    }
+}
