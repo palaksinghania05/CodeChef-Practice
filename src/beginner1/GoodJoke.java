6fb3a38bea109c563013ee6a938d85e6486c17a6
Palak Singhania <singhaniapalak2002@gmail.com>
2023-04-24 23:47:16 +0530

    modified two question

--- src/beginner1/GoodJoke.java
+++ src/beginner1/GoodJoke.java
@@ -1,4 +1,22 @@
 package beginner1;
 
+import java.util.Scanner;
+
 public class GoodJoke {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            int result = 0;
+            for (int i = 0; i < N; i++) {
+                int c1 = sc.nextInt();
+                int c2 = sc.nextInt();
+            }
+            for (int i = 1; i <= N; i++)
+                result ^= i;
+            System.out.println(result);
+            T--;
+        }
+    }
 }
--- src/beginner1/LittleElephantAndCandies.java
+++ src/beginner1/LittleElephantAndCandies.java
@@ -1,7 +1,27 @@
 package beginner1;
 
+import java.util.Scanner;
+
 public class LittleElephantAndCandies {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            int C = sc.nextInt();
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int totalCandies = 0;
+            for (int i = 0; i < N; i++) {
+                totalCandies += arr[i];
+            }
+            if (C >= totalCandies)
+                System.out.println("Yes");
+            else
+                System.out.println("No");
+            T--;
+        }
     }
 }
