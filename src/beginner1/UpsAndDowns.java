4d4c3471c58cf42f18b9cb3498e1a4878ae1b709
Palak Singhania <singhaniapalak2002@gmail.com>
2023-05-13 21:35:49 +0530

    modified one question

--- src/beginner1/UpsAndDowns.java
+++ src/beginner1/UpsAndDowns.java
@@ -1,7 +1,32 @@
 package beginner1;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class UpsAndDowns {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++) {
+                arr[i] = sc.nextInt();
+            }
+            Arrays.sort(arr);
+            int start = 0;
+            int end = N - 1;
+            while (start < end) {
+                System.out.print(arr[start] + " ");
+                System.out.print(arr[end] + " ");
+                start++;
+                end--;
+            }
+            if (N % 2 == 0)
+                System.out.println();
+            else
+                System.out.println(arr[start]);
+            T--;
+        }
     }
 }
