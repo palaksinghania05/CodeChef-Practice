eabc7f95a4d0685bed406809f8b70a03b5b50666
Palak Singhania <singhaniapalak2002@gmail.com>
2023-05-17 23:11:15 +0530

    modified one question

--- src/beginner1/FarmerFeb.java
+++ src/beginner1/FarmerFeb.java
@@ -1,7 +1,37 @@
 package beginner1;
 
+import java.util.Scanner;
+
 public class FarmerFeb {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int i = 1;
+            while (true) {
+                if (isPrime(x + y + i)) {
+                    break;
+                } else {
+                    i++;
+                }
+            }
+            System.out.println(i);
+            T--;
+        }
+    }
 
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
