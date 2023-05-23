9b3b923801a2fa3d1ee2ef3f40edfaf13f2ef4c7
Palak Singhania <singhaniapalak2002@gmail.com>
2023-05-23 21:52:40 +0530

    modified one question

--- src/beginner1/HungryChef.java
+++ src/beginner1/HungryChef.java
@@ -1,4 +1,25 @@
 package beginner1;
 
+import java.util.Scanner;
+
 public class HungryChef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+            int N = sc.nextInt();
+            int R = sc.nextInt();
+            if (R < (X * N)) {
+                System.out.println(-1);
+            } else {
+                int b = (R - N * X) / (Y - X);
+                b = Math.min(b, N);
+                int a = N - b;
+                System.out.println(a + " " + b);
+            }
+            T--;
+        }
+    }
 }
