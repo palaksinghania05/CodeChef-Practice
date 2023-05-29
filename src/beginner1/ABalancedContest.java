982c794d5d5093e8e937e041fd1c70e8770bbba2
Palak Singhania <singhaniapalak2002@gmail.com>
2023-05-29 23:41:34 +0530

    modified one question

--- src/beginner1/ABalancedContest.java
+++ src/beginner1/ABalancedContest.java
@@ -1,5 +1,28 @@
 package beginner1;
 
-public class ABalancedContest {
+import java.util.Scanner;
 
+public class ABalancedContest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            int P = sc.nextInt();
+            int[] arr = new int[N];
+            int cakewalk = 0, hard = 0;
+            for (int i = 0; i < N; i++) {
+                arr[i] = sc.nextInt();
+                if (arr[i] >= (P / 2))
+                    cakewalk++;
+                else if (arr[i] <= (P / 10))
+                    hard++;
+            }
+            if ((cakewalk == 1) && (hard == 2))
+                System.out.println("yes");
+            else
+                System.out.println("no");
+            T--;
+        }
+    }
 }
