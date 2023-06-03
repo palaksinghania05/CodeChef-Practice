c4cfea1f625d84af19dc4036608085e64598e8e8
Palak Singhania <singhaniapalak2002@gmail.com>
2023-06-03 15:41:05 +0530

    modified one question

--- src/beginner1/Plus2Minus1.java
+++ src/beginner1/Plus2Minus1.java
@@ -1,7 +1,15 @@
 package beginner1;
 
+import java.util.Scanner;
+
 public class Plus2Minus1 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            long N = sc.nextLong();
+            System.out.println(0 + " " + N);
+            T--;
+        }
     }
 }
