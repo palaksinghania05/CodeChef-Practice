dd02046bec54bd738810f2df68f83741549a603d
Palak Singhania <singhaniapalak2002@gmail.com>
2023-06-05 19:00:13 +0530

    modified one question

--- src/beginner1/StickBreak.java
+++ src/beginner1/StickBreak.java
@@ -1,4 +1,20 @@
 package beginner1;
 
+import java.util.Scanner;
+
 public class StickBreak {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int L = sc.nextInt();
+            int K = sc.nextInt();
+            if (L % K == 0) {
+                System.out.println("0");
+            } else if (L % K != 0) {
+                System.out.println("1");
+            }
+            T--;
+        }
+    }
 }
