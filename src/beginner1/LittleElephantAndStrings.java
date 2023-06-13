277ad8d41c04a77f00e10a3804b59b4a883f29f1
Palak Singhania <singhaniapalak2002@gmail.com>
2023-06-13 23:13:09 +0530

    modified one question

--- src/beginner1/LittleElephantAndStrings.java
+++ src/beginner1/LittleElephantAndStrings.java
@@ -1,4 +1,34 @@
 package beginner1;
 
+import java.util.Scanner;
+
 public class LittleElephantAndStrings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        String A[] = new String[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.next();
+        }
+        while (K > 0) {
+            String s = sc.next();
+            if (s.length() >= 47) {
+                System.out.println("Good");
+            } else {
+                int flag = 0;
+                for (int j = 0; j < N; j++) {
+                    if (s.contains(String.valueOf(A[j]))) {
+                        flag = 1;
+                    }
+                }
+                if (flag == 1) {
+                    System.out.println("Good");
+                } else {
+                    System.out.println("Bad");
+                }
+            }
+            K--;
+        }
+    }
 }
