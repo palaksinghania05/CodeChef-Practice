2b6cafaecaa8efbd7e3faae635d36a362640f701
Palak Singhania <singhaniapalak2002@gmail.com>
2023-06-27 23:50:50 +0530

    modified one question

--- src/beginner1/ChefVsDoof.java
+++ src/beginner1/ChefVsDoof.java
@@ -1,7 +1,23 @@
 package beginner1;
 
+import java.util.Scanner;
+
 public class ChefVsDoof {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T >= 0) {
+            int N = sc.nextInt();
+            int result = 1;
+            for (int i = 0; i < N; i++) {
+                result *= sc.nextInt();
+            }
+            if (result % 2 == 0) {
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+            }
+            T--;
+        }
     }
 }
--- src/beginner1/TheGreatRun.java
+++ src/beginner1/TheGreatRun.java
@@ -1,7 +1,36 @@
 package beginner1;
 
+import java.util.Scanner;
+
 public class TheGreatRun {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int A = sc.nextInt();
+            int K = sc.nextInt();
+            int[] arr = new int[A];
+            int sum = 0;
+            for (int i = 0; i < A; i++) {
+                arr[i] = sc.nextInt();
+                if (i < K) {
+                    sum += arr[i];
+                }
+            }
+            int max = sum;
+            int i = 0;
+            int j = K;
+            while (j < A) {
+                sum -= arr[i];
+                i++;
+                sum += arr[j];
+                j++;
+                if (sum > max) {
+                    max = sum;
+                }
+            }
+            System.out.println(max);
+            T--;
+        }
     }
 }
