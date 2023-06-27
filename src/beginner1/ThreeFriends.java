2a91ffb001964e7ed81420e01c70fae31c017a29
Palak Singhania <singhaniapalak2002@gmail.com>
2023-06-28 00:20:49 +0530

    modified one question

--- src/beginner1/ThreeFriends.java
+++ src/beginner1/ThreeFriends.java
@@ -1,7 +1,30 @@
 package beginner1;
 
+import java.util.Scanner;
+
 public class ThreeFriends {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+            int Z = sc.nextInt();
+            if (X + Y - Z == 0)
+                System.out.println("yes");
+            else if (X - Y - Z == 0)
+                System.out.println("yes");
+            else if (-X - Y - Z == 0)
+                System.out.println("yes");
+            else if (-X + Y - Z == 0)
+                System.out.println("yes");
+            else if (-X - Y + Z == 0)
+                System.out.println("yes");
+            else if (X - Y + Z == 0)
+                System.out.println("yes");
+            else
+                System.out.println("no");
+            T--;
+        }
     }
 }
