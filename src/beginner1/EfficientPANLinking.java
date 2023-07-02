49e1dfb495f273ef99406cb7f32abca6fe5e8630
Palak Singhania <singhaniapalak2002@gmail.com>
2023-07-02 19:36:29 +0530

    modified one question

--- src/beginner1/EfficientPANLinking.java
+++ src/beginner1/EfficientPANLinking.java
@@ -1,7 +1,17 @@
 package beginner1;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class EfficientPANLinking {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            BigInteger N = sc.nextBigInteger();
+            BigInteger c = N.mod(BigInteger.valueOf(20));
+            System.out.println(c);
+            T--;
+        }
     }
 }
