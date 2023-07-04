1ab9c5f332d5f6df3a49fe0c48fb2f05154d544c
Palak Singhania <singhaniapalak2002@gmail.com>
2023-07-04 20:26:04 +0530

    modified one question

--- src/beginner1/FromHeavenToEarth.java
+++ src/beginner1/FromHeavenToEarth.java
@@ -1,7 +1,23 @@
 package beginner1;
 
+import java.util.Scanner;
+
 public class FromHeavenToEarth {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            int V1 = sc.nextInt();
+            int V2 = sc.nextInt();
+            double root = N * Math.sqrt(2);
+            double doubled = 2 * N;
+            double stairs = root / V1;
+            double elevator = doubled / V2;
+            if (stairs < elevator) {
+                System.out.println("Stairs");
+            } else System.out.println("Elevator");
+            T--;
+        }
     }
 }
