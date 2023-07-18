05e23138a9520b1ef3f469a7abe09e04aae9b167
Palak Singhania <singhaniapalak2002@gmail.com>
2023-07-18 20:17:08 +0530

    modified one question

--- src/beginner1/TraceofMatrix.java
+++ src/beginner1/TraceofMatrix.java
@@ -1,7 +1,45 @@
 package beginner1;
 
+import java.util.Scanner;
+
 public class TraceofMatrix {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            int mat[][] = new int[N][N];
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    mat[i][j] = sc.nextInt();
+                }
+            }
+            int r = 0, c = 0;
+            for (int i = 0; i < N; i++) {
+                r = 0;
+                c = i;
+                int trace = 0;
+                while (r < N && c < N) {
+                    trace += mat[r][c];
+                    max = Math.max(max, trace);
+                    r++;
+                    c++;
+                }
+            }
+            for (int i = 1; i < N; i++) {
+                r = i;
+                c = 0;
+                int trace = 0;
+                while (r < N && c < N) {
+                    trace += mat[r][c];
+                    max = Math.max(max, trace);
+                    r++;
+                    c++;
+                }
+            }
+            System.out.println(max);
+            T--;
+        }
     }
 }
