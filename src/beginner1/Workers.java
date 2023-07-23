ff8636d8dad0d79ae3ef24037ab236ca0bfb1b17
Palak Singhania <singhaniapalak2002@gmail.com>
2023-07-23 15:45:16 +0530

    modified one question

--- src/beginner1/Workers.java
+++ src/beginner1/Workers.java
@@ -1,7 +1,44 @@
 package beginner1;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Workers {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        int[] arr2 = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < N; i++) {
+            arr2[i] = sc.nextInt();
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            if (!map.containsKey(arr2[i])) {
+                map.put(arr2[i], arr[i]);
+            } else {
+                if (arr[i] < map.get(arr2[i])) {
+                    map.put(arr2[i], arr[i]);
+                }
+            }
+        }
+        if (map.containsKey(1) && map.containsKey(2) && map.containsKey(3)) {
+            int a = map.get(1) + map.get(2);
+            int b = map.get(3);
+            if (a < b) {
+                System.out.println(a);
+            } else if (b < a) {
+                System.out.println(b);
+            } else {
+                System.out.println(b);
+            }
+        } else if (!map.containsKey(3)) {
+            System.out.println(map.get(1) + map.get(2));
+        } else if (!map.containsKey(1) || !map.containsKey(2)) {
+            System.out.println(map.get(3));
+        }
     }
 }
