dcdd52bc3558320025d77351e92cecd2e7ed0a3d
Palak Singhania <singhaniapalak2002@gmail.com>
2023-07-25 17:22:22 +0530

    modified one question

--- src/beginner1/DevuAndAnArray.java
+++ src/beginner1/DevuAndAnArray.java
@@ -1,4 +1,35 @@
 package beginner1;
 
+import java.util.Scanner;
+
 public class DevuAndAnArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int Q = sc.nextInt();
+        int[] arr = new int[N];
+        for (int j = 0; j < N; j++) {
+            arr[j] = sc.nextInt();
+        }
+        int minimum = arr[0];
+        for (int j = 0; j < N; j++) {
+            if (minimum > arr[j]) {
+                minimum = arr[j];
+            }
+        }
+        int maximum = Integer.MIN_VALUE;
+        for (int j = 0; j < N; j++) {
+            if (maximum < arr[j]) {
+                maximum = arr[j];
+            }
+        }
+        for (int j = 0; j < Q; j++) {
+            int t = sc.nextInt();
+            if (t >= minimum && t <= maximum) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
 }
