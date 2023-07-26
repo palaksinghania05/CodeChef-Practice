cf3728feeed6228ab71e10881f59f34960af6757
Palak Singhania <singhaniapalak2002@gmail.com>
2023-07-26 17:33:10 +0530

    modified one question

--- src/beginner1/LazySalesman.java
+++ src/beginner1/LazySalesman.java
@@ -1,7 +1,30 @@
 package beginner1;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class LazySalesman {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            int W = sc.nextInt();
+            int nums[] = new int[N];
+            for (int i = 0; i < N; i++) {
+                nums[i] = sc.nextInt();
+            }
+            Arrays.sort(nums);
+            int wages = 0;
+            int count = 0;
+            for (int i = N - 1; i >= 0; i--) {
+                wages += nums[i];
+                count++;
+                if (wages >= W)
+                    break;
+            }
+            System.out.println(N - count);
+            T--;
+        }
     }
 }
