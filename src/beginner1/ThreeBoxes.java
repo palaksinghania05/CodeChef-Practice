8119315c7eeab1d7cc4172e8108fb9297eaab4d6
Palak Singhania <singhaniapalak2002@gmail.com>
2023-08-23 23:52:18 +0530

    added one question

---
+++ src/beginner1/ThreeBoxes.java
@@ -0,0 +1,27 @@
+package beginner1;
+
+import java.util.Scanner;
+
+public class ThreeBoxes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            int C = sc.nextInt();
+            int D = sc.nextInt();
+            if ((A + B + C) <= D)
+                System.out.println(1);
+            else if ((A + B) <= D)
+                System.out.println(2);
+            else if ((C + B) <= D)
+                System.out.println(2);
+            else if ((A + C) <= D)
+                System.out.println(2);
+            else
+                System.out.println(3);
+            T--;
+        }
+    }
+}
