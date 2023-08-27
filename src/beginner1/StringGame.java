2a94d950669e5d29d8b7c1baf4b27bf4367a1359
Palak Singhania <singhaniapalak2002@gmail.com>
2023-08-27 16:47:21 +0530

    Update StringGame.java

--- src/beginner1/StringGame.java
+++ src/beginner1/StringGame.java
@@ -2,7 +2,7 @@ package beginner1;
 
 import java.util.Scanner;
 
-public class ABooks {
+public class StringGame {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 		int t= sc.nextInt();
