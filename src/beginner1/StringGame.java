36029f1acff55aad3ea94c7b07e99ecd676f6e36
Palak Singhania <singhaniapalak2002@gmail.com>
2023-09-07 22:17:05 +0530

    modified two question

--- src/beginner1/StringGame.java
+++ src/beginner1/StringGame.java
@@ -1,30 +1,31 @@
 package beginner1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class StringGame {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-		int t= sc.nextInt();
-		for(int i=0;i<t;i++){
-		    int n= sc.nextInt();
-		    char ch[] = sc.next().toCharArray();
-		    Arrays.sort(ch);
-		    if(n%2!=0){
-		        System.out.println("NO");
-		        continue;
-		    }
-		    char f='0';
-		    for(int j=0;j<n;j+=2){
-		        if(ch[j]!=ch[j+1]){
-		            f='1';
-		            break;
-		        }
-		    }
-		    if(f=='0')
-		        System.out.println("YES");
-		    else
-		        System.out.println("NO");
-		}
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            char ch[] = sc.next().toCharArray();
+            Arrays.sort(ch);
+            if (n % 2 != 0) {
+                System.out.println("NO");
+                continue;
+            }
+            char f = '0';
+            for (int j = 0; j < n; j += 2) {
+                if (ch[j] != ch[j + 1]) {
+                    f = '1';
+                    break;
+                }
+            }
+            if (f == '0')
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
     }
 }
--- src/shortContests/sept2023/SlowStart.java
+++ src/shortContests/sept2023/SlowStart.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class SlowStart {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        /*Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         while (T > 0) {
             int X = sc.nextInt();
@@ -17,6 +17,30 @@ public class SlowStart {
                 else
                     damage += X;
                 turns++;
+            }
+            System.out.println(turns);
+            T--;
+        }*/
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int X = sc.nextInt();
+            int H = sc.nextInt();
+            int half = X / 2;
+            int turns = 0;
+            for (int i = 0; i < 5; i++) {
+                if (H > 0) {
+                    H -= half;
+                    turns++;
+                } else {
+                    break;
+                }
+            }
+            if (H > 0) {
+                while (H > 0) {
+                    H -= X;
+                    turns++;
+                }
             }
             System.out.println(turns);
             T--;
