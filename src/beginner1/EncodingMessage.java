26bd7cf462df59cdfb811eb28287a03f2fe58811
Palak Singhania <singhaniapalak2002@gmail.com>
2023-09-17 22:12:13 +0530

    modified one question

--- src/beginner1/EncodingMessage.java
+++ src/beginner1/EncodingMessage.java
@@ -12,57 +12,48 @@ public class EncodingMessage {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-        while(T>0){
+        while (T > 0) {
             int N = sc.nextInt();
             String S = sc.next();
-            if(N%2==0)
-                S = firstStep(S);
-            else
-                S = firstStep(S.substring(0,N-1)) + S.charAt(N-1);
-            String result = secondStep(S);
+            char[] arr = S.toCharArray();
+            if (N % 2 == 1) {
+                for (int i = 0; i < N - 1; i = i + 2) {
+                    char temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            } else {
+                for (int i = 0; i < N; i = i + 2) {
+                    char temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+            int[] x = new int[26];
+            int[] y = new int[26];
+            int[][] z = new int[26][2];
+            for (int j = 0; j < 26; j++) {
+                x[j] = j + 97;
+            }
+            for (int j = 0; j < 26; j++) {
+                y[j] = x[25 - j];
+            }
+            for (int j = 0; j < 26; j++) {
+                z[j][0] = x[j];
+                z[j][1] = y[j];
+            }
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < N; i++) {
+                int p = arr[i];
+                for (int k = 0; k < 26; k++) {
+                    if (p == z[k][0]) {
+                        arr[i] = (char) z[k][1];
+                        result.append(arr[i]);
+                    }
+                }
+            }
             System.out.println(result);
             T--;
         }
-    }
-
-    public static String firstStep(String s){
-        String out = "";
-        for (int i = 0; i < s.length(); i+=2) {
-            out+=s.charAt(i+1);
-            out+=s.charAt(i);
-        }
-        return out;
-    }
-
-    public static String secondStep(String s){
-        for (int i = 0; i < s.length(); i++) {
-            s = s.replace("a", "z");
-            s = s.replace("b","y");
-            s = s.replace("c","x");
-            s = s.replace("d","w");
-            s = s.replace("e","v");
-            s = s.replace("f","u");
-            s = s.replace("g","t");
-            s = s.replace("h","s");
-            s = s.replace("i","r");
-            s = s.replace("j","q");
-            s = s.replace("k","p");
-            s = s.replace("l","o");
-            s = s.replace("m","n");
-            s = s.replace("n","m");
-            s = s.replace("o","l");
-            s = s.replace("p","k");
-            s = s.replace("q","j");
-            s = s.replace("r","i");
-            s = s.replace("s","h");
-            s = s.replace("t","g");
-            s = s.replace("u","f");
-            s = s.replace("v","e");
-            s = s.replace("w","d");
-            s = s.replace("x","c");
-            s = s.replace("y","b");
-            s = s.replace("z","a");
-        }
-        return s;
     }
 }
