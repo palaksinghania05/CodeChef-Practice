285d2203f5b429b6203d37c5c17978228d08d71e
Palak Singhania <singhaniapalak2002@gmail.com>
2023-10-23 21:31:00 +0530

    added one question

---
+++ src/advanced1/NewPiece.java
@@ -0,0 +1,25 @@
+package advanced1;
+
+import java.util.Scanner;
+
+public class NewPiece {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            int P = sc.nextInt();
+            int Q = sc.nextInt();
+            if ((A == P) && (B == Q))
+                System.out.println(0);
+            else {
+                if ((A + B + P + Q) % 2 == 0)
+                    System.out.println(2);
+                else
+                    System.out.println(1);
+            }
+            T--;
+        }
+    }
+}
