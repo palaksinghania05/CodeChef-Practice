19dd2d5d522e6beadc4b653b57dc0346207edcb4
Palak Singhania <singhaniapalak2002@gmail.com>
2023-11-14 22:52:57 +0530

    added one question

---
+++ src/advanced1/DisabledKing.java
@@ -0,0 +1,19 @@
+package advanced1;
+
+import java.util.Scanner;
+
+public class DisabledKing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            if (N % 2 == 0) {
+                System.out.println(N);
+            } else {
+                System.out.println(N - 1);
+            }
+            T--;
+        }
+    }
+}
