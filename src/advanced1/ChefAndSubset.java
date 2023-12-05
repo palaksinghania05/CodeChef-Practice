21d3d49c3df857b742530097a6ed675e3f892af7
Palak Singhania <singhaniapalak2002@gmail.com>
2023-12-05 22:49:45 +0530

    added one question

---
+++ src/advanced1/ChefAndSubset.java
@@ -0,0 +1,26 @@
+package advanced1;
+
+import java.util.Scanner;
+
+public class ChefAndSubset {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int a, b, c, d;
+            a = sc.nextInt();
+            b = sc.nextInt();
+            c = sc.nextInt();
+            d = sc.nextInt();
+
+            if (a == 0 || b == 0 || c == 0 || d == 0 || a + b == 0 || a + c == 0 || a + d == 0 ||
+                    a + b + c == 0 || a + b + d == 0 || a + c + d == 0 || a + b + c + d == 0 || b + c == 0 || b + d == 0 ||
+                    b + c + d == 0 || c + d == 0) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+            T--;
+        }
+    }
+}
