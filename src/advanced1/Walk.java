c0e97944fd702268047cfb75128a5c10a8d1927d
Palak Singhania <singhaniapalak2002@gmail.com>
2023-12-20 21:55:24 +0530

    added one question

---
+++ src/advanced1/Walk.java
@@ -0,0 +1,22 @@
+package advanced1;
+
+import java.util.Scanner;
+
+public class Walk {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++)
+                arr[i] = sc.nextInt();
+            int result = 0;
+            for (int i = 0; i < N; i++) {
+                result = Math.max(result, arr[i] + i);
+            }
+            System.out.println(result);
+            T--;
+        }
+    }
+}
