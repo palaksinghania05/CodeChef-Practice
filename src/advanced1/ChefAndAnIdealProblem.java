5dd9c1ef3639112a49e0238023ab2b95079fc904
Palak Singhania <singhaniapalak2002@gmail.com>
2024-06-24 23:31:09 +0530

    added one question

---
+++ src/advanced1/ChefAndAnIdealProblem.java
@@ -0,0 +1,17 @@
+package advanced1;
+
+import java.util.Scanner;
+
+public class ChefAndAnIdealProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = 3;
+        System.out.println(X);
+        int Y = sc.nextInt();
+        if (Y == 2) {
+            System.out.println(1);
+        } else {
+            System.out.println(2);
+        }
+    }
+}
