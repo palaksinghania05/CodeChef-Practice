d09ba8c7c259efe1dc6844e436992ac8d1c04c5f
Palak Singhania <singhaniapalak2002@gmail.com>
2025-04-22 23:48:52 +0530

    added one question

--- src/beginner/MiamiGP.java
+++ src/beginner/MiamiGP.java
@@ -10,6 +10,12 @@ import java.util.Scanner;
 
 public class MiamiGP {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+            System.out.println(((X * 1.07) < Y) ? "NO" : "YES");
+        }
     }
 }
