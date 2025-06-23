1c13f4d99972ea63dc17662f2fd535c602ef7e15
Palak Singhania <singhaniapalak2002@gmail.com>
2025-06-23 23:16:59 +0530

    added one question

--- src/beginner2/BellaCiao.java
+++ src/beginner2/BellaCiao.java
@@ -6,17 +6,11 @@ public class BellaCiao {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
+        StringBuilder result = new StringBuilder();
         while (T-- > 0) {
-            long D = sc.nextLong();
-            long d = sc.nextLong();
-            long p = sc.nextLong();
-            long Q = sc.nextLong();
-            long temp1 = D / d;
-            long temp2 = D % d;
-            long temp3 = d * temp1 * (2 * p + (temp1 - 1) * Q) / 2;
-            long temp4 = temp2 * (p + temp1 * Q);
-            long result = temp3 + temp4;
-            System.out.println(result);
+            long D = sc.nextInt(), d = sc.nextInt(), P = sc.nextInt(), Q = sc.nextInt(), temp1 = D / d, temp2 = D % d;
+            result.append(D * P + temp1 * (temp1 - 1) / 2 * Q * d + temp2 * temp1 * Q).append("\n");
         }
+        System.out.print(result);
     }
 }
